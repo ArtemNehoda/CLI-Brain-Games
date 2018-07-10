8b1a772afbb5c7c510c584c5840d4f34b3d3d581
Artem Nehoda <negoda1995@bk.ru>
2018-07-10 18:34:31 +0100

    10/07/18 Artem

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+import {actual} from '../index'
 console.log('Welcome to the Brain Games!');
+   const name = actual('May I have your name?');
+    console.log('Hallo,'+name+'!');
