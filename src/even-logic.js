9b4752067b65b7dbbd062658e23ad9a02113502d
Artem Nehoda <negoda1995@bk.ru>
2018-07-12 19:58:35 +0100

    change even logic

--- src/even-logic.js
+++ src/even-logic.js
@@ -4,7 +4,10 @@ let three = 0;
 export const getRandomNum = (min, max) => (Math.floor(Math.random() * (max - min + 1)) + min);
 export const isEvenNumber = num => (num % 2 === 0 ? 'yes' : 'no');
 export const game = (userName) => {
-  if (three >= 3) return console.log(`Congratulations , ${userName}`);
+  if (three >= 3) {
+    console.log(`Congratulations , ${userName}`);
+    return;
+  }
   const num = getRandomNum(1, 1000);
   const correctAnswer = isEvenNumber(num);
   const userAnswer = actual(`Question : ${num}`);
