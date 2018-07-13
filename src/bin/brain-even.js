dba77fdf03f5a500a145e0257643d5bfea9b9652
Artem Nehoda <negoda1995@bk.ru>
2018-07-13 18:15:37 +0100

    gfdh

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -5,4 +5,6 @@ import { game } from '../even-logic';
 console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".');
 const name = actual('May I have your name?');
 console.log(` Hallo, ${name}!`);
+
+
 game(name);
--- src/even-logic.js
+++ src/even-logic.js
@@ -5,17 +5,17 @@ export const getRandomNum = (min, max) => (Math.floor(Math.random() * (max - min
 export const isEvenNumber = num => (num % 2 === 0 ? 'yes' : 'no');
 export const game = (userName) => {
   if (three >= 3) {
-    console.log(`Congratulations , ${userName}`);
+    console.log(`Congratulations , ${userName}!`);
     return;
   }
   const num = getRandomNum(1, 1000);
   const correctAnswer = isEvenNumber(num);
   const userAnswer = actual(`Question : ${num}\nYour answer:`);
   const gameOverString = `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.
-    \nLet's try again , ${userName}!`;
+      \nLet's try again , ${userName}!`;
   if (correctAnswer === userAnswer) {
     three += 1;
     console.log('Correct!');
     game(userName);
-  } else if (!correctAnswer === userAnswer)console.log(gameOverString);
+  } else console.log(gameOverString);
 };
