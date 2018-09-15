1bbab7a271c5814d88cea6d2413304adbe8cc87c
Artem Nehoda <negoda1995@bk.ru>
2018-09-15 11:23:40 +0100

    fix isPrime

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -4,13 +4,9 @@ import getRandomNumber from '../utilFunc';
 
 const gameDescription = 'Is this number prime?';
 
-const isPrime = (num, divisor = num - 1) => {
-  if ((num !== 2 && num % 2 === 0) || num <= 0) return false;
-  if (num === 1 || divisor === 1) return true;
-  if (num % divisor !== 0) {
-    return isPrime(num, divisor - 1);
-  }
-  return null;
+const isPrime = (num) => {
+  for (let i = 2; i < num; i++) { if (num % i === 0) return false; }
+  return num !== 1 && num !== 0;
 };
 
 const gameInit = () => {
